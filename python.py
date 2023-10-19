ea7f7a5a5b0e5d7997430f7c4f5be6c99948372a
alirecommends <145074424+alitmu@users.noreply.github.com>
2023-10-18 21:43:53 -0400

    final

---
+++ python.py
@@ -0,0 +1,8 @@
+month=input("enter month")
+
+if month=="December" or month=="January" or month=="February" or month=="March" or month=="April":
+    print("Winter is here.")
+elif month=="May" or month=="June" or month=="July" or month=="August":
+    print("Summer is here.")
+else:
+    print("Winter is close.")

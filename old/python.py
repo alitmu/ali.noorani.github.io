a142bdefe300f91915fcca2daf4ec126569f1b1d
alirecommends <145074424+alitmu@users.noreply.github.com>
2023-11-30 19:17:30 -0500

    moved website

--- old/python.py
+++
@@ -1,6 +0,0 @@
-import requests
-response = requests.get("https://swapi.dev/api/people/1/")
-data = response.json()
-print(data["mass"])
-
-# it calls Name value of the list from above url. response: 77
